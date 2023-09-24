138f3997b665776a37def534d611f7dac5405c28
Darkroom01 <128232134+Darkroom01@users.noreply.github.com>
2023-09-24 18:56:46 +0900

    FBV로 블로그 포스트 상세페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,15 @@ def index(request):
             'posts' : posts,
         }
     )
+
+
+def single_post_page(reqeust, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        reqeust,
+        'blog/single_post_page.html',
+        {
+            'post' : post,
+        }
+    )
